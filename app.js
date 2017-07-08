79bd572d712e4debdce6d5b2493cf748d7c54873
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-07-08 01:57:52 -0700

    Add controllers and routing add views

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
 // module
-var weatherApp = angular.module('weatherApp', []);
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+
+
+// Config routes
+weatherApp.config(function($routeProvider){
+	$routeProvider
+		.when('/',{
+			templateUrl: '/pages/home.htm',
+			controller: 'homeController'
+		})
+		.when('/forcast',{
+			templateUrl: '/pages/forcast.htm',
+			controller: 'forcastController'
+		})
+
+
+});
+
+
+// HomePage controller
+weatherApp.controller('homeController', ['$scope','$log',function($scope,$log){
+
+	$scope.say = 'homePageController'
+	//$log.log($scope.say);
+
+}]);
+
+
+
+// Forcast controller
+weatherApp.controller('forcastController', ['$scope','$log',function($scope,$log){
+
+
+
+}]);
+
+
